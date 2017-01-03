e45520c8b984beb21863e0da93be8835d3cb2bb0
Amy Parent <amy@amyparent.com>
2017-01-03 13:16:50 +0100

    Remove now obsolete orbit_hashmap

--- src/tests-vm/tests-vm.c
+++ src/tests-vm/tests-vm.c
@@ -5,7 +5,6 @@
 //  Created by Cesar Parent on 24/12/2016.
 //  Copyright © 2016 cesarparent. All rights reserved.
 //
-#include "orbit_hashmap.h"
 #include "orbit_string.h"
 #include "orbit_double.h"
 #include "orbit_vtable.h"
@@ -13,9 +12,6 @@
 #define DECL_TEST(test) {#test, test}
 
 TEST_LIST = {
-    DECL_TEST(hashmap_create),
-    DECL_TEST(hashmap_insert),
-    DECL_TEST(hashmap_grow),
     DECL_TEST(string_create),
     DECL_TEST(string_hash),
     DECL_TEST(double_hash),
