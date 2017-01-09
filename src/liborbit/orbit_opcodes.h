c5dd613055da788960db94b3e87910bba8d701d9
Amy Parent <amy@amyparent.com>
2017-01-09 13:07:03 +0000

    Add operand count to opcode list

--- src/liborbit/orbit_opcodes.h
+++ src/liborbit/orbit_opcodes.h
@@ -10,33 +10,37 @@
 //  declaration of the opcodes defines how the main interpreter loop's jump
 //  table is laid out, and should be changed only if necessary.
 //
+//  The first parameter is the opcode's mnemonic, the second the number of
+//  bytes following the opcode in the bytecode stream that should be read as
+//  argument.
+//
 #ifndef OPCODE
-#define OPCODE(_)
+#define OPCODE(_, _)
 #endif
 
-OPCODE(halt)            /// Halts the virtual machine.
-OPCODE(const_nil)       /// 
-OPCODE(const_true)      /// 
-OPCODE(const_false)     /// 
-OPCODE(load_const)      /// Pushes a constant from the pool onto the stack.
-OPCODE(load_local)      /// Pushes a local variable onto the stack.
-OPCODE(load_field)      /// Pushes an object's field onto the stack.
-OPCODE(load_global)     /// Pushes a global variable onto the stack.
-OPCODE(store_local)     /// Stores the top of the stack in a local variable.
-OPCODE(store_field)     /// Stores the top of the stack in an object's field.
-OPCODE(store_global)    /// Stores the top of the stack into a global variable.
+OPCODE(halt, 0)         /// Halts the virtual machine.
+OPCODE(const_nil, 0)    /// 
+OPCODE(const_true, 0)   /// 
+OPCODE(const_false, 0)  /// 
+OPCODE(load_const, 1)   /// Pushes a constant from the pool onto the stack.
+OPCODE(load_local, 1)   /// Pushes a local variable onto the stack.
+OPCODE(load_field, 1)   /// Pushes an object's field onto the stack.
+OPCODE(load_global, 1)  /// Pushes a global variable onto the stack.
+OPCODE(store_local, 1)  /// Stores the top of the stack in a local variable.
+OPCODE(store_field, 1)  /// Stores the top of the stack in an object's field.
+OPCODE(store_global, 1) /// Stores the top of the stack into a global variable.
 
-OPCODE(and)             ///
-OPCODE(or)              ///
+OPCODE(and, 1)          ///
+OPCODE(or, 1)           ///
 
 /**/
 
-OPCODE(jump_if)         ///
-OPCODE(jump)            ///
-OPCODE(rjump_if)        ///
-OPCODE(rjump)           ///
-OPCODE(pop)             ///
-OPCODE(swap)            ///
+OPCODE(jump_if, 1)      ///
+OPCODE(jump, 1)         ///
+OPCODE(rjump_if, 1)     ///
+OPCODE(rjump, 1)        ///
+OPCODE(pop, 0)          ///
+OPCODE(swap, 0)         ///
 
 /*
  * Invocation codes - dynamic run-time dispatch means the argument is an index
@@ -44,10 +48,10 @@ OPCODE(swap)            ///
  * in the VM's dispatch table
  */
 
-OPCODE(invoke)          /// Invoke a function
-OPCODE(ret_val)         ///
-OPCODE(ret)             ///
-OPCODE(init)            ///
-OPCODE(debug_prt)       ///
+OPCODE(invoke, 1)       /// Invoke a function
+OPCODE(ret_val, 1)      ///
+OPCODE(ret, 0)          ///
+OPCODE(init, 1)         ///
+OPCODE(debug_prt, 1)    ///
 
 #undef OPCODE
