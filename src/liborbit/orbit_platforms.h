66dd652a48418ecdd7ed24fc8f9aa76918e94473
Amy Parent <amy@amyparent.com>
2017-02-10 15:16:13 +0000

    Add platform names in orbit_platform.h

--- src/liborbit/orbit_platforms.h
+++ src/liborbit/orbit_platforms.h
@@ -8,6 +8,25 @@
 #ifndef OrbitPlatforms_h
 #define OrbitPlatforms_h
 
+#ifdef _WIN32
+#define ORBIT_PLATFORM "Windows"
+#elif __APPLE__
+#include "TargetConditionals.h"
+#if TARGET_IPHONE_SIMULATOR
+#define ORBIT_PLATFORM "iOS-x86"
+#elif TARGET_OS_IPHONE
+#define ORBIT_PLATFORM "iOS-arm"
+#elif TARGET_OS_MAC
+#define ORBIT_PLATFORM "macOS"
+#endif
+#elif __linux__
+#define ORBIT_PLATFORM "Linux"
+#elif __unix__
+#define ORBIT_PLATFORM "UNIX"
+#else
+#define ORBIT_PLATFORM "Unknown Platform"
+#endif
+
 #if __STDC_VERSION__ >= 199901L
 #define ORBIT_FLEXIBLE_ARRAY_MEMB   
 #else
