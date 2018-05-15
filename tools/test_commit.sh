c1603bea9d7464b37035a941d8f0597efc3fca71
Amy Parent <amy@amyparent.com>
2018-05-15 12:19:08 +0100

    Add test-running pre commit hook

---
+++ tools/test_commit.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+SOURCE_DIR=$1
+BUILD_DIR="${SOURCE_DIR}/.build-tests.ignore"
+
+mkdir $BUILD_DIR && cd $BUILD_DIR
+cmake $SOURCE_DIR -DCMAKE_BUILD_TYPE=Release
+make all > /dev/null
+make test
+RESULT=$?
+rm -rf $BUILD_DIR
+[ $RESULT -ne 0 ] && exit 1 
+exit 0
