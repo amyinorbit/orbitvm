edb450c5a3c157f865e325ed71343fe9587577e5
Amy Parent <amy@amyparent.com>
2018-05-15 13:03:15 +0100

    Fix commit test script in-source build
    If the test build directory existed already, the pre-commit script
    would trigger an in-source build, polluting the repository with
    CMake cache files

--- tools/git-hooks/run-tests.sh
+++ tools/git-hooks/run-tests.sh
@@ -2,7 +2,7 @@
 SOURCE_DIR=$1
 BUILD_DIR="${SOURCE_DIR}/.build-tests.ignore"
 
-mkdir $BUILD_DIR && cd $BUILD_DIR
+mkdir -p $BUILD_DIR && cd $BUILD_DIR
 cmake $SOURCE_DIR -DCMAKE_BUILD_TYPE=Release
 make all > /dev/null
 make test
