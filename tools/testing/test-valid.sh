4e4564db75008aa1bd380cbe397f40d900e6a25a
Amy Parent <amy@amyparent.com>
2018-05-15 12:32:54 +0100

    Fix test runner script, permissions on pre-commit

--- tools/testing/test-valid.sh
+++ tools/testing/test-valid.sh
@@ -5,13 +5,11 @@ FILES=`find $1 -type f -name '*.orbit' -print`
 RESULT=0
 
 for i in $FILES; do
-    echo "$2 $i"
     $2 $i
     if [ $? != 0 ]; then
         RESULT=1
     fi
 done
 
-exit $RESULT
-
-#return find $1 -type f -name '*.orbit' -exec $2 {} \;
+[ $RESULT -ne 0 ] && exit 1
+exit 0
