f4027f97c65b2a409b5c6e6c769af4624d40de70
Amy Parent <amy@amyparent.com>
2018-05-15 21:09:59 +0100

    Move diagnostics to libOrbitAST

---
+++ libs/csupport/diag.c
@@ -0,0 +1,123 @@
+//===--------------------------------------------------------------------------------------------===
+// orbit/csupport/diag.c - Main implementation of the Orbit diagnostic engine.
+// This source is part of Orbit - Compiler Support
+//
+// Created on 2018-05-08 by Amy Parent <amy@amyparent.com>
+// Copyright (c) 2016-2018 Amy Parent <amy@amyparent.com>
+// Licensed under the MIT License
+// =^•.•^=
+//===--------------------------------------------------------------------------------------------===
+#include <stdint.h>
+#include <stdarg.h>
+#include <orbit/csupport/console.h>
+#include <orbit/csupport/diag.h>
+#include <orbit/utils/assert.h>
+
+OrbitDiagManager orbit_defaultDiagManager;
+
+void _orbit_defaultDiagConsumer(OCSource* source, OrbitDiag* diagnostic) {
+    
+    // print basic stuff first:
+    console_setColor(stderr, CLI_BOLD);
+    fputs("----- ", stderr);
+    switch(diagnostic->level) {
+        case ORBIT_DIAGLEVEL_INFO:
+            fputs("info", stderr);
+            break;
+        case ORBIT_DIAGLEVEL_WARN:
+            console_setColor(stderr, CLI_YELLOW);
+            fputs("warning", stderr);
+            break;
+        case ORBIT_DIAGLEVEL_ERROR:
+            console_setColor(stderr, CLI_RED);
+            fputs("error", stderr); 
+            break;
+    }
+    console_setColor(stderr, CLI_RESET);
+    console_setColor(stderr, CLI_BOLD);
+    fprintf(stderr, " in %s -----\n", source->path);
+    
+    console_setColor(stderr, CLI_RESET);
+    if(diagnostic->sourceLoc.line != 0) {
+        console_printSourceLocLine(stderr, source, diagnostic->sourceLoc);
+        console_printCaret(stderr, diagnostic->sourceLoc, CLI_GREEN);
+    }
+    
+    char* printed = orbit_diagGetFormat(diagnostic);
+    fprintf(stderr, "%s\n\n", printed);
+    orbit_dealloc(printed);
+}
+
+OrbitDiagID orbit_diagEmitError(OCSourceLoc loc, const char* format, int count, ...) {
+    OrbitDiagID id = orbit_diagNew(
+        &orbit_defaultDiagManager,
+        ORBIT_DIAGLEVEL_ERROR,
+        format
+    );
+    orbit_diagAddSourceLoc(id, loc);
+    
+    va_list args;
+    va_start(args, count);
+    for(int i = 0; i < count; ++i) {
+        orbit_diagAddParam(id, va_arg(args, OrbitDiagArg));
+    }
+    va_end(args);
+    
+    return id;
+}
+
+void orbit_diagManagerInit(OrbitDiagManager* manager, OCSource* source) {
+    OASSERT(manager, "Invalid Diagnostics Manager instance");
+    manager->source = source;
+    manager->consumer = &_orbit_defaultDiagConsumer;
+    manager->errorCount = 0;
+    manager->diagnosticCount = 0;
+}
+
+OrbitDiagID orbit_diagNew(OrbitDiagManager* manager, OrbitDiagLevel level, const char* format) {
+    OASSERT(manager, "Diagnostics manager does not exist");
+    OASSERT(manager->diagnosticCount < ORBIT_DIAG_MAXCOUNT, "Diagnostics overflow");
+    if(level >= ORBIT_DIAGLEVEL_ERROR) {
+        manager->errorCount += 1;
+    }
+    uint32_t id = manager->diagnosticCount++;
+    OrbitDiag* d = &manager->diagnostics[id];
+    d->sourceLoc.line = d->sourceLoc.column = 0;
+    d->level = level;
+    d->format = format;
+    d->paramCount = 0;
+    return (OrbitDiagID){.manager = manager, .id=id};
+}
+
+void orbit_diagAddParam(OrbitDiagID id, OrbitDiagArg param) {
+    OASSERT(id.manager, "Diagnostics manager does not exist");
+    
+    OrbitDiag* d = &id.manager->diagnostics[id.id];
+    OASSERT(d->paramCount < 10, "Diagnostics are limited 10 parameters");
+    
+    d->params[d->paramCount] = param;
+    d->paramCount += 1;
+}
+
+void orbit_diagAddSourceLoc(OrbitDiagID id, OCSourceLoc loc) {
+    OASSERT(id.manager, "Diagnostics manager does not exist");
+    OrbitDiag* d = &id.manager->diagnostics[id.id];
+    d->sourceLoc = loc;
+}
+
+void orbit_diagEmitAll(OrbitDiagManager* manager) {
+    orbit_diagEmitAbove(manager, ORBIT_DIAGLEVEL_INFO);
+    manager->diagnosticCount = 0;
+}
+
+void orbit_diagEmitAbove(OrbitDiagManager* manager, OrbitDiagLevel level) {
+    OASSERT(manager, "Diagnostics manager does not exist");
+    // TODO: Sort diagnostics by severity, location?
+    
+    for(uint32_t i = 0; i < manager->diagnosticCount; ++i) {
+        OrbitDiag* d = &manager->diagnostics[i];
+        if(d->level < level) { continue; }
+        manager->consumer(manager->source, d);
+    }
+}
+
---
+++ libs/csupport/diag_formatter.c
@@ -0,0 +1,141 @@
+//===--------------------------------------------------------------------------------------------===
+// orbit/csupport/diag_formatter.c - Implementation of the diagnostics token formatter
+// This source is part of Orbit - Compiler Support
+//
+// Created on 2018-05-08 by Amy Parent <amy@amyparent.com>
+// Copyright (c) 2016-2018 Amy Parent <amy@amyparent.com>
+// Licensed under the MIT License
+// =^•.•^=
+//===--------------------------------------------------------------------------------------------===
+#include <stdio.h>
+#include <string.h>
+#include <orbit/csupport/diag.h>
+#include <orbit/csupport/string.h>
+#include <orbit/utils/assert.h>
+
+static const char* scan(const char* str, const char* end, char value) {
+    while(*str != value && str != end) {
+        str += 1;
+    }
+    return str;
+}
+
+static bool isDigit(codepoint_t c) {
+    return c >= '0' && c <= '9';
+}
+
+static bool modifierIs(const char* str, uint32_t length, const char* value) {
+    if(length != strlen(value)) { return false; }
+    return strncmp(str, value, length) == 0;
+}
+
+static void handleSelect(OCStringBuffer* buf, const char* arg, uint32_t length, int value) {
+    OASSERT(arg, "diagnostic format argument: no parameter");
+    OASSERT(length > 0, "diagnostic format argument: no parameter");
+    const char* end = arg + length;
+    while(value) {
+        const char* next = scan(arg, end, '|');
+        OASSERT(next != end, "diagnostic format argument: invalid selection index");
+        value -= 1;
+        arg = next+1;
+    }
+    
+    end = scan(arg, end, '|');
+    orbit_stringBufferAppendC(buf, arg, end-arg);
+}
+
+static void handleS(OCStringBuffer* buf, int value) {
+    if(value == 1) { return; }
+    orbit_stringBufferAppend(buf, 's');
+}
+
+static void handlePlural(OCStringBuffer* buf, const char* arg, uint32_t length, int value) {
+    handleSelect(buf, arg, length, value == 1 ? 0 : 1);
+}
+
+// After a few attempts using rec-descent parsers, this is heavily based on clang's diagnostic
+// formatter (lines ~700+ -> https://clang.llvm.org/doxygen/Basic_2Diagnostic_8cpp_source.html)
+char* orbit_diagGetFormat(OrbitDiag* diag) {
+    // TODO: probably cache this? Though we are unlikely to print diags more than once
+    // TODO: We should be safe, but we should probably port all of this to codepoint_t
+    
+    OCStringBuffer buffer, *buf = &buffer;
+    orbit_stringBufferInit(buf, 512);
+    const char* fmt = diag->format;
+    const char* end = diag->format + strlen(diag->format);
+    
+    while(fmt != end) {
+                
+        // If we aren't looking at a $, we can just copy until the first dollar (or the end).
+        if(*fmt != '$') {
+            const char* strEnd = scan(fmt, end, '$');
+            orbit_stringBufferAppendC(buf, fmt, strEnd-fmt);
+            fmt = strEnd;
+            continue;
+        }
+        
+        // Skip the dollar.
+        fmt += 1;
+        
+        const char* modifier = NULL;
+        const char* parameter = NULL;
+        uint16_t modifierLength = 0;
+        uint16_t parameterLength = 0;
+        
+        if(!isDigit(*fmt)) {
+            modifier = fmt;
+            while(*fmt >= 'a' && *fmt <= 'z') { fmt += 1; }
+            modifierLength = fmt - modifier;
+            
+            if(*fmt == '{') {
+                fmt += 1;
+                parameter = fmt;
+                fmt = scan(fmt, end, '}');
+                OASSERT(fmt != end, "diagnostic format string: unmatched '{'");
+                parameterLength = fmt - parameter;
+                fmt += 1;
+            }
+        }
+        
+        OASSERT(isDigit(*fmt), "diagnostic format argument: should be a digit");
+        uint8_t argIdx = *fmt - '0';
+        OASSERT(argIdx < diag->paramCount, "diagnostic format argument: invalid index");
+        OrbitDiagArg param = diag->params[argIdx];
+        fmt += 1;
+        
+        switch(param.kind) {
+        case ORBIT_DPK_INT:
+            if(modifierIs(modifier, modifierLength, "s")) {
+                handleS(buf, param.intValue);
+            }
+            else if(modifierIs(modifier, modifierLength, "plural")) {
+                handlePlural(buf, parameter, parameterLength, param.intValue);
+            }
+            else if(modifierIs(modifier, modifierLength, "select")) {
+                handleSelect(buf, parameter, parameterLength, param.intValue);
+            }
+            else {
+                OASSERT(!modifier, "diagnostic format argument: unknown modifier");
+                char number[32];
+                int numberLength = snprintf(number, 32, "%d", param.intValue);
+                orbit_stringBufferAppendC(buf, number, numberLength);
+            }
+            break;
+            
+        case ORBIT_DPK_CSTRING:
+            OASSERT(!modifier, "diagnostic format argument: no string modifiers");
+            orbit_stringBufferAppendC(buf, param.cstringValue, strlen(param.cstringValue));
+            break;
+            
+        case ORBIT_DPK_STRING: {
+                OCString* str = orbit_stringPoolGet(param.stringValue);
+                OASSERT(str, "diagnostic format argument: invalid string pool ID");
+                OASSERT(!modifier, "diagnostic format argument: no string modifiers");
+                orbit_stringBufferAppendC(buf, str->data, str->length);
+            }
+            break;
+        }
+    }
+    
+    return buffer.data; // Ownership of the allocated data string is transferred
+}
